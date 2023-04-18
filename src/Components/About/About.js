3e67093bdcd0725374809f4a91b5b3e9c7323195
Radu Catalin <crcata19@yahoo.com>
2023-04-18 15:58:41 +0300

    Remove text in about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -27,7 +27,7 @@ export default function About() {
                             based in Tg. Mures, Romania 📍
                         </h3>
                         <p className='mulish'>
-                            As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Bootstrap, and SCSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.
+                            As a Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Bootstrap, and SCSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.
                         </p>
                     </div>
 
