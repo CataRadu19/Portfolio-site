5d5b24c3e511c2d998b60360cfdcfa8fcb33b341
Radu Catalin <crcata19@yahoo.com>
2023-05-16 12:43:34 +0300

    Added loader and queries for mobile

--- src/App.js
+++ src/App.js
@@ -6,19 +6,43 @@ import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export default function App() {
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  // 1 sec loader
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      {isLoading ? <FontAwesomeIcon icon={faSpinner} className='fa-spinner' spin style={{
+        color: "#152239",
+        position: "absolute",
+        top: "40%",
+        left: "46%",
+        fontSize: "7rem",
+
+      }} /> :
+        <div className="App">
+          <Header />
+          <Hero />
+          <About />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      }
+    </>
   );
 }
 
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,5 +1,5 @@
 import './Hero.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import wave from '../../Assets/Images/wave.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -11,14 +11,36 @@ import react from '../../Assets/Images/react.svg'
 import bootstrap from '../../Assets/Images/bootstrap.svg'
 import scss from '../../Assets/Images/scss.svg'
 import glasses from '../../Assets/Images/glasses.png'
+import Typewriter from 'typewriter-effect';
 
 export default function Hero() {
 
     const [smart, setSmart] = useState(false);
 
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 1000);
+        };
+
+        // Initial check
+        handleResize();
+
+        // Event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const handleSmart = () => {
         setSmart(!smart);
     }
+
+
     return (
         <div className="hero" id='home'>
             <div className='container-width'>
@@ -26,7 +48,14 @@ export default function Hero() {
                 <div className='hero-main-container'>
                     <div className='hero-text-container'>
                         <div className='hero-title-text'>
-                            <h1>Front-End React Developer <img src={wave} alt='Catalin.dev' /></h1>
+                            {isMobile ? <h1>Front-End React Developer</h1> : <h1><Typewriter options={{
+                                autoStart: true,
+                                loop: true,
+                                delay: 50,
+                                deleteSpeed: 30,
+                                pauseFor: 1500,
+                                strings: ['Front-End Developer', 'React Sorcerer', 'Team Player',]
+                            }} /> <img src={wave} alt='Catalin.dev' /></h1>}
                         </div>
 
                         {smart ? <p className='mulish'>I am a solution-oriented Front-End Developer, lifelong learner, and adventurer. I enjoy development because of the satisfaction I get by overcoming challenges. I am motivated by the opportunity that software provides to positively impact the life of an individual, and the world as a whole.
